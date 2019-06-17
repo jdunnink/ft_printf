8efbb05b36c33ffd0a1a03a0e91ac1dd343171de
Jesse Dunnink <jdunnink@f0r4s13.codam.nl>
2019-06-17 16:22:06 +0200

    printf validated

--- srcs/test_main.c
+++ srcs/test_main.c
@@ -15,25 +15,23 @@ int     char_random_test(int max_width, int min, int max);
 int     string_random_test(int max_width, int max_precis, int min, int max);
 int     ptr_random_test(int max_width);
 
+/*
+
+static long double ldouble_rand(long double min, long double max)
+{
+    long double scale;
+    
+    scale = rand() / (long double) RAND_MAX;
+    return (min + scale * ( max - min ));
+}
+
+*/
 
 static int test_double(size_t test_num, long long int max_range)
 {
     while (test_num > 0)
     {
         if(double_random_test(80, 80, max_range) == -1)
-            return (-1);
-        test_num--;
-    }
-    return (1);
-}
-
-/*
-
-static int test_ldouble(size_t test_num, long long int max_range)
-{
-    while (test_num > 0)
-    {
-        if(ldouble_random_test(80, 80, max_range) == -1)
             return (-1);
         test_num--;
     }
@@ -117,7 +115,6 @@ static int test_ptr(size_t test_num)
     return (1);
 }
 
-*/
 
 int main(void)
 {
@@ -125,42 +122,40 @@ int main(void)
 
     int test_size;
 
-    test_size = 1000;
+    test_size = 10000;
 
-    if(test_double(test_size, 1) == - 1)
+    if(test_double(test_size / 10, 1) == - 1)
         return (0);
 
-    if(test_double(test_size, 10) == - 1)
+    if(test_double(test_size / 10, 10) == - 1)
         return (0);
 
-    if(test_double(test_size, 100) == - 1)
+    if(test_double(test_size / 10, 100) == - 1)
         return (0);
 
-    if(test_double(test_size, 1000) == - 1)
+    if(test_double(test_size / 10 , 1000) == - 1)
         return (0);
 
-    if(test_double(test_size, 10000) == - 1)
+    if(test_double(test_size / 10, 10000) == - 1)
         return (0);
 
-    if(test_double(test_size, 100000) == - 1)
+    if(test_double(test_size / 10, 100000) == - 1)
         return (0);
 
-    if(test_double(test_size, 1000000) == - 1)
+    if(test_double(test_size / 10, 1000000) == - 1)
         return (0);
 
-    if(test_double(test_size, 10000000) == - 1)
+    if(test_double(test_size / 10, 10000000) == - 1)
         return (0);
 
-    if(test_double(test_size, 100000000) == - 1)
+    if(test_double(test_size / 10, 100000000) == - 1)
         return (0);
 
-    if(test_double(test_size, 1000000000) == - 1)
+    if(test_double(test_size / 10, 1000000000) == - 1)
         return (0);
 
-    if(test_double(test_size, 10000000000) == - 1)
+    if(test_double(test_size / 10, 10000000000) == - 1)
         return (0);
-
-/*
 
     if(test_int(test_size) == - 1)
         return (0);
@@ -183,16 +178,99 @@ int main(void)
     if (test_ptr(test_size * 10) == -1)
         return (0);
 
-*/
+    /*
+
     int res;
     int real_res;
 
-    long double test;
-    
-    test = 0.53343453453453455235345345345;
+    res = ft_printf("%+-#28.48Lf\n", (long double)LLONG_MAX);
+    real_res = printf("%+-#28.48Lf\n", (long double)LLONG_MAX);
 
-    res = ft_printf("%+-#28.48Lf\n", test);
-    real_res = printf("%+-#28.48Lf\n", test);
+    if (res != real_res)
+        return (0);
+
+    res = ft_printf("%+-#28.48Lf\n", (long double)LLONG_MIN);
+    real_res = printf("%+-#28.48Lf\n", (long double)LLONG_MIN);
+
+    if (res != real_res)
+        return (0);
+
+    res = ft_printf("%+-#28.48Lf\n", (long double)ULLONG_MAX);
+    real_res = printf("%+-#28.48Lf\n", (long double)ULLONG_MAX);
+
+    if (res != real_res)
+        return (0);
+
+    res = ft_printf("%+-#28.48Lf\n", ((long double)ULLONG_MAX * -1));
+    real_res = printf("%+-#28.48Lf\n", ((long double)ULLONG_MAX * -1));
+
+    if (res != real_res)
+        return (0);
+
+    long double test;
+    long double min;
+    long double max;
+    
+    test = 0;
+    min = -1;
+    max = 1;
+    res = 0;
+    real_res = 0;
+    while (res == real_res && min > ((long double)ULLONG_MAX * -1) && max < (long double)ULLONG_MAX)
+    {
+        test = ldouble_rand(min, max);
+        res = ft_printf("%+-#28.48Lf\n", test);
+        real_res = printf("%+-#28.48Lf\n", test);
+        min = min * 1.01;
+        max = max * 1.01;
+    }
+
+    max = 1;
+    min = -1;
+    int count;
+    count = 100000;
+
+    while (res == real_res && count > 0)
+    {
+        test = ldouble_rand(min, max);
+        res = ft_printf("%+#28.15Lf\n", test);
+        real_res = printf("%+#28.15Lf\n", test);
+        count--;
+    }
+
+    while (res == real_res && count < 1000000 && max < ULLONG_MAX)
+    {
+        test = ldouble_rand(min, max);
+        res = ft_printf("%+#28.18Lf\n", test);
+        real_res = printf("%+#28.18Lf\n", test);
+        min = min * 1.01;
+        max = max * 1.01;
+        count++;
+    }
+
+    min = -10;
+    max = 10;
+
+    while (res == real_res && count > 0)
+    {
+        test = ldouble_rand(min, max);
+        res = ft_printf("%+#28.100Lf\n", test);
+        real_res = printf("%+#28.100Lf\n", test);
+        count--;
+    }
+
+    min = min * 10;
+    max = max * 10;
+
+    while (res == real_res && count < 1000)
+    {
+        test = ldouble_rand(min, max);
+        res = ft_printf("%+#28.50Lf\n", test);
+        real_res = printf("%+#28.50Lf\n", test);
+        count++;
+    }
+
+    */
 
     return (0);
 }
