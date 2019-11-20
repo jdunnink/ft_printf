0b6607dbcbc152da31679d1bc04d10d3cd18876e
Jesse Dunnink <jdunnink@f1r4s1.codam.nl>
2019-11-20 14:07:50 +0100

    readme added

--- libftprintf/srcs/ft_printf.c
+++ libftprintf/srcs/ft_printf.c
@@ -12,6 +12,16 @@
 
 #include "printf.h"
 
+/*
+**	The main function of this library, ft_printf is able to print
+**	a variadic number of arguments given as elements to be accessed sequentially
+**	in the order specified in the input string 'format'. ft_print will
+**	look for as many arguments within the variadic list as the count of '%'s
+**	within the format string. The return value for this function is the 
+**	total number of characters printed or the 'real' length of the format
+**	string with converted arguments included.
+*/
+
 int		ft_printf(const char *format, ...)
 {
 	char	*dest;
